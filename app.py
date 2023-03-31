991d6f563d3140d98492251e7575dbd22c685b32
Jayzing00 <jayleo2106@gmail.com>
2023-04-01 01:33:10 +0200

    cleaning for deployment

--- app.py
+++ app.py
@@ -24,5 +24,6 @@ def analyze_sentiment():
     score = result[0]["score"]
     return jsonify({"sentiment": sentiment, "score": score})
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000, debug=True)
+
