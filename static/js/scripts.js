e732385bf1ee234ba6928906428835d5823ca343
Jayzing00 <jayleo2106@gmail.com>
2023-04-02 19:41:26 +0200

    added color for analysis result

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -9,8 +9,25 @@ $(document).ready(function () {
                 $("#result").addClass("d-none");
             },
             success: function (response) {
-                $("#sentiment").text(response.sentiment);
+                let sentimentText = '';
+                let color;
+                switch (response.sentiment) {
+                    case 'LABEL_0':
+                        sentimentText = 'Negative';
+                        color = `rgba(${response.score*255}, 0, 0, ${response.score})`;
+                        break;
+                    case 'LABEL_1':
+                        sentimentText = 'Neutral';
+                        color = `rgba(0, 0, ${response.score*255}, ${response.score})`;
+                        break;
+                    case 'LABEL_2':
+                        sentimentText = 'Positive';
+                        color = `rgba(0, ${response.score*255}, 0, ${response.score})`;
+                        break;
+                }
+                $("#sentiment").text(sentimentText);
                 $("#score").text(response.score.toFixed(2));
+                $("#color-box").css("background-color", color);
                 $("#result").removeClass("d-none");
             },
             error: function (error) {
